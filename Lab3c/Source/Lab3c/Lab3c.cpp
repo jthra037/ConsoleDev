40f634bf7cdba9b8a13cede27fb14a6ee56b1f44
Jacob-Laptop <its_thrasher@live.ca>
2017-10-02 12:32:12 -0400

    Remove old project

--- Lab3c/Source/Lab3c/Lab3c.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Lab3c.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Lab3c, "Lab3c" );
